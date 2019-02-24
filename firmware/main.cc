a79f6d0592dcde207fec73a78e3566ddc9259f76
Henner Zeller <h.zeller@acm.org>
2019-02-24 00:20:55 -0800

    Update missing glyphs for updated UI. Add sleep mode on indicator off.
    o Update needed characters in fonts for all necessary elements of the UI.
    o When we detect the dial indicator stop clocking data (i.e. it is
      switched off), go to sleep. We still draw about 10μA, but that is
      mostly due to the OLED display. Disconnecting the OLED from GND
      makes current consumption drop to 0.8μA. Maybe add another transistor
      to disconnect OLED ?

--- firmware/main.cc
+++ firmware/main.cc
@@ -17,11 +17,16 @@
 #include <avr/sleep.h>
 #include <stdint.h>
 #include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
 
 #include "ssd1306-display.h"
 
 #include "font-bignumber.h"
 #include "font-smalltext.h"
+#include "font-tinytext.h"
 
 constexpr float d_mm = 50.0f;
 
@@ -125,6 +130,24 @@ const char *strfmt(char *buffer, uint8_t buflen, int32_t v,
     return buffer + 1;
 }
 
+EMPTY_INTERRUPT(PCINT0_vect);
+static void SleepTillLevelChange() {
+  cli();
+  GIMSK |= (1<<PCIE);   // level change interrupt
+  PCMSK = CLK_BIT;       // Switch on for PB4, which is the indicator clock.
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+  sleep_enable();
+  sei();
+  sleep_cpu();
+
+  // Zzzz...
+
+  // Waking up due to interrupt.
+  sleep_disable();
+  GIMSK = 0;
+}
+
 int main() {
     _delay_ms(100);  // Let display warm up and get ready before the first i2c
     SSD1306Display display;
@@ -132,8 +155,21 @@ int main() {
     char buffer[16];
     uint8_t x;
     DialData last_dial_data;
+    uint32_t off_cycles = 0;
+    constexpr uint32_t kPowerOffAfterCycles = 150;
     for (;;) {
       DialData dial_data = readDialIndicator();
+
+      if (dial_data.off)
+        off_cycles++;
+      else
+        off_cycles = 0;
+
+      if (off_cycles > kPowerOffAfterCycles) {
+        display.SetOn(false);
+        SleepTillLevelChange();
+        display.SetOn(true);
+      }
 
       if (last_dial_data.off != dial_data.off
           || last_dial_data.negative != dial_data.negative
@@ -141,20 +177,34 @@ int main() {
           || (last_dial_data.value == 0) != (dial_data.value == 0)) {
         display.ClearScreen();  // Visuals will change. Clean-slatify.
       }
-      last_dial_data = dial_data;
 
       if (dial_data.off) {
-        // TODO: go to sleep.
-        display.WriteString(&progmem_font_smalltext.meta, 16, 0,
-                            "-------------");
+        if (!last_dial_data.off) {  // Only write to display the
+          display.WriteString(&progmem_font_smalltext.meta, 0, 0,
+                              "©Henner Zeller");
+          display.WriteString(&progmem_font_tinytext.meta, 0, 16,
+                              "GNU Public License");
+          display.WriteString(&progmem_font_tinytext.meta, 0, 32,
+                              "github.com/hzeller/");
+          display.WriteString(&progmem_font_tinytext.meta, 0, 48,
+                              "digi-spherometer");
+        }
       }
       else if (dial_data.value == 0) {
+        display.WriteString(&progmem_font_smalltext.meta, 48, 0,
+                            "flat");
         display.WriteString(
-          &progmem_font_bignumber.meta, 0, 16, "0000");  // "flat"
+          &progmem_font_bignumber.meta, 34, 16, "OK");
       }
       else if (!dial_data.negative) {
-        display.WriteString(&progmem_font_smalltext.meta, 16, 0,
-                            "-Z0-"); // "Please zero on flat surface"
+        display.WriteString(&progmem_font_smalltext.meta, 0, 8,
+                            "Please zero on");
+        display.WriteString(&progmem_font_smalltext.meta, 8, 32,
+                            "flat surface");
+      }
+      else if (dial_data.value == last_dial_data.value
+               && dial_data.is_imperial == last_dial_data.is_imperial) {
+        // Nothing changed. No need to update display.
       }
       else {
         int32_t value = dial_data.value;
@@ -165,15 +215,16 @@ int main() {
         x = display.WriteString(&progmem_font_smalltext.meta, x, 0,
                                 strfmt(buffer, sizeof(buffer),
                                        value,
-                                       dial_data.is_imperial ? 5 : 3, 10));
+                                       dial_data.is_imperial ? 5 : 3, 7));
         display.WriteString(&progmem_font_smalltext.meta, x, 0,
                             dial_data.is_imperial ? "\"  " : "mm");
 
+        display.WriteString(&progmem_font_smalltext.meta, 0, 40, "r");
         float sag = dial_data.is_imperial ? value / 100000.0f : value / 1000.0f;
         if (dial_data.is_imperial) {
           int32_t radius = 10 * calc_r_inch(sag);
           if (radius > 9999) {   // Limit digits to screen-size
-            display.WriteString(&progmem_font_smalltext.meta, 0, 16, "m");
+            display.WriteString(&progmem_font_smalltext.meta, 0, 16, ">");
             radius = 9999;
           } else {
             display.WriteString(&progmem_font_smalltext.meta, 0, 16, " ");
@@ -181,12 +232,12 @@ int main() {
           x = display.WriteString(
             &progmem_font_bignumber.meta, 12, 16,
             strfmt(buffer, sizeof(buffer), radius, 1, 5));
-          display.WriteString(&progmem_font_smalltext.meta, x, 16, "\"");
+          display.WriteString(&progmem_font_bignumber.meta, x, 16, "\"");
         }
         else {
           int32_t radius = calc_r_mm(sag);
           if (radius > 9999) { // Limit digits to screen-size
-            display.WriteString(&progmem_font_smalltext.meta, 0, 16, "m");
+            display.WriteString(&progmem_font_smalltext.meta, 0, 16, ">");
             radius = 9999;
           } else {
             display.WriteString(&progmem_font_smalltext.meta, 0, 16, " ");
@@ -197,5 +248,6 @@ int main() {
           display.WriteString(&progmem_font_smalltext.meta, x, 40, "mm");
         }
       }
+      last_dial_data = dial_data;
     }
 }
