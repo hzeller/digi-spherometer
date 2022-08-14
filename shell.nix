761587ef5dbaf926bf2f6cf545e11e21434aabb1
Henner Zeller <h.zeller@acm.org>
2022-08-13 22:28:24 -0700

    Add dependencies for easy local development set-up.

---
+++ shell.nix
@@ -0,0 +1,14 @@
+# This is a nix-shell for use with the nix package manager.
+# If you have nix installed, you may simply run `nix-shell`
+# in this repo, and have all dependencies ready in the new shell.
+
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = with pkgs;
+    [
+      kicad
+      pkgsCross.avr.buildPackages.gcc
+      avrdude
+      openscad
+    ];
+}
